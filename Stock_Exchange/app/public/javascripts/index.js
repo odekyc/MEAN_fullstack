e080ec17bee47c866133b2e1800acb9c287bfa2f
odekyc <odekyc@gmail.com>
2017-03-11 23:42:34 -0800

    hi

--- Stock_Exchange/app/public/javascripts/index.js
+++ Stock_Exchange/app/public/javascripts/index.js
@@ -3,14 +3,17 @@ $(document).ready(function() {
         if($(this).val() === 'ch-simplified'){
              $(".english").css("visibility","hidden");
              $(".ch-simplified").css("visibility","visible");
+             $(".ch-traditional").css("visibility","hidden");
         }
         else if( $(this).val() === 'en' ){
              $(".english").css("visibility","visible");
              $(".ch-simplified").css("visibility","hidden");
+             $(".ch-traditional").css("visibility","hidden");
         }
         else if( $(this).val() === 'ch-traditional' ){
              $(".english").css("visibility","hidden");
-             $(".ch-simplified").css("visibility","visible");
+             $(".ch-simplified").css("visibility","hidden");
+             $(".ch-traditional").css("visibility","visible");
         }
     });
 });

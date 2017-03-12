3f23b9e22b6ca7d4ba03ec7c143d4e6ea88d100c
odekyc <odekyc@gmail.com>
2017-03-11 22:02:10 -0800

    hi

--- Stock_Exchange/app/public/javascripts/index.js
+++ Stock_Exchange/app/public/javascripts/index.js
@@ -4,5 +4,10 @@ $(document).ready(function() {
              $(".english").css("visibility","hidden");
              $(".ch-simplified").css("visibility","visible");
         }
+        else if ( $(this).val() === 'english' ){
+          alert("hi");
+             $(".english").css("visibility","visible");
+             $(".ch-simplified").css("visibility","hidden");
+        }
     });
 });

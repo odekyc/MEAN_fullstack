5201c208b3a0a65d5ad38b35b901aedde5f03455
odekyc <odekyc@gmail.com>
2017-03-11 20:54:05 -0800

    hi

--- Stock_Exchange/app/public/javascripts/index.js
+++ Stock_Exchange/app/public/javascripts/index.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
     $('[name=language]').change(function(){
-        if($(this).val() === 'en'){
-             alert("hi");
+        if($(this).val() === 'ch-simplified'){
+             $(".english").css("visibility","hidden");
+             $(".ch-simplified").css("visibility","visible");
         }
     });
 });
